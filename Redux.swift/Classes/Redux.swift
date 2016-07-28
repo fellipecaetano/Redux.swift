7f27a3c2a43034d9a9d36e288f9ce9dd451d4791
Fellipe Caetano <fellipe.caetano@movile.com>
2016-07-28 20:15:12 -0300

    Project-wide linting

--- Redux.swift/Classes/Redux.swift
+++ Redux.swift/Classes/Redux.swift
@@ -1,21 +1,21 @@
 import Foundation
 
 /**
- The data structure responsible for holding application state, allowing controlled mutation through dispatched 
+ The data structure responsible for holding application state, allowing controlled mutation through dispatched
  `Actions` and notifying interested parties that `subscribe` to state changes.
  */
 public final class Store<State>: Publisher, Dispatch {
     private let reduce: (State, Action) -> State
-    
+
     private var state: State {
         didSet { publish(state) }
     }
-    
+
     private var subscribers: [String: State -> Void]
-    
+
     /**
      Initializes a `Store`.
-     
+
      - parameter initialState: The initial value of the application state in hold.
      - parameter reducer: The root pure function that's responsible for transforming state according to `Actions`.
     */
@@ -24,47 +24,47 @@ public final class Store<State>: Publisher, Dispatch {
         state = initialState
         subscribers = [:]
     }
-    
+
     /**
      Perform state changes described by the action and the root reducer.
-     
+
      - parameter action: The descriptor of **what** is the state change.
      */
     public func dispatch(action: Action) {
         state = reduce(state, action)
     }
-    
+
     /**
      Registers a handler that's called when state changes
-     
+
      - parameter subscription: A closure that's called whenever there's a change to the state in hold.
      - returns: A closure that unsubscribes the provided subscription.
      */
-    public func subscribe(subscription: State -> Void) -> Void -> Void {
+    public func subscribe(subscription: State -> Void) -> (Void -> Void) {
         let token = NSUUID().UUIDString
         subscribers[token] = subscription
-        
+
         subscription(state)
-        
+
         return { [weak self] in
             self?.subscribers.removeValueForKey(token)
         }
     }
-    
+
     private func publish(newState: State) {
         subscribers.values.forEach { $0(newState) }
     }
 }
 
 /**
- Defines `Action` dispatch cabalities. Instances conforming to `Dispatch` are expected to know how to 
+ Defines `Action` dispatch cabalities. Instances conforming to `Dispatch` are expected to know how to
  dispatch `Actions`.
  */
 
 public protocol Dispatch {
     /**
      Dispatches an action.
-     
+
      - parameter action: The action that'll be dispatched.
     */
     func dispatch(action: Action)
@@ -73,7 +73,7 @@ public protocol Dispatch {
 extension Dispatch {
     /**
      Executes a closure with an injected `dispatch` function. Useful for asynchronous `Action` dispatching.
-     
+
      - parameter thunk: The closure that will be executed with an injected `dispatch` function.
      */
     public func dispatch(thunk: (Action -> Void) -> Void) {
@@ -87,14 +87,14 @@ extension Dispatch {
 public protocol Action {}
 
 /**
- Instances conforming to `Publisher` are expected to know how to add handlers that are provided with an associated 
+ Instances conforming to `Publisher` are expected to know how to add handlers that are provided with an associated
  object in response to generic events.
  */
 public protocol Publisher {
     associatedtype Publishing
     /**
      Adds a handler to a generic event.
-     
+
      - parameter subscription: The handler that will be called in response to generic events.
      - returns: A closure that unsubscribes the provided subscription.
     */
@@ -104,11 +104,11 @@ public protocol Publisher {
 extension Publisher {
     /**
      Adds the handler defined by a `Subscriber` that is compatible with the events defined by this `Publisher`.
-     
+
      - parameter subscriber: The compatible `Subscriber`.
      - returns: A closure that unsubscribes the provided `Subscriber`.
      */
-    public func subscribe <T: Subscriber where T.Publishing == Publishing> (subscriber subscriber: T) -> Void -> Void {
+    public func subscribe <T: Subscriber where T.Publishing == Publishing> (subscriber subscriber: T) -> (Void -> Void) {
         return subscribe { newState in
             subscriber.receive(subscriber.select(newState))
         }
@@ -118,7 +118,7 @@ extension Publisher {
 extension Publisher where Self: Dispatch {
     /**
      Subscribes a `Subscriber` that is compatible with this `Publisher`.
-     
+
      - parameter subscriber: The compatible `Subscriber`.
      - returns: A `dispatch` function and an `unsubscribe` handle, encapsulated.
     */
@@ -131,24 +131,24 @@ extension Publisher where Self: Dispatch {
 
 /**
  Instances conforming to `Subscriber` receive objects of an `associatedtype` associated to generic updates.
- Plus, they should be able to select some portion of this associated object, producing a second associated 
+ Plus, they should be able to select some portion of this associated object, producing a second associated
  object of another `associatedtype`.
  */
 public protocol Subscriber: class {
     associatedtype Publishing
     associatedtype Selection
-    
+
     /**
      Selects some portion of an object of an arbitrary (associated) type.
-     
+
      - parameter publishing: The object that will suffer the selection.
      - returns: An object selected from `publishing`.
     */
     func select(publishing: Publishing) -> Selection
-    
+
     /**
      Receives an object associated to a generic update, preferably after undergoing selection by `select`.
-     
+
      - parameter selection: The object associated to a generic update, after going through selection.
      */
     func receive(selection: Selection)
@@ -167,16 +167,16 @@ public protocol StateConnection: Dispatch {
 private struct AnyStateConnection: StateConnection {
     private let doDispatch: Action -> Void
     private let doUnsubscribe: Void -> Void
-    
+
     private init (dispatch: Action -> Void, unsubscribe: Void -> Void) {
         doDispatch = dispatch
         doUnsubscribe = unsubscribe
     }
-    
+
     func dispatch(action: Action) {
         doDispatch(action)
     }
-    
+
     func unsubscribe() {
         doUnsubscribe()
     }
@@ -188,7 +188,7 @@ private struct AnyStateConnection: StateConnection {
 public protocol StateConnectable: class {
     /**
      Receives a connection.
-     
+
      - parameter connection: The connection to receive.
     */
     func connect(with connection: StateConnection)
@@ -197,16 +197,16 @@ public protocol StateConnectable: class {
 extension StateConnectable where Self: Subscriber {
     /**
      Receives a connection created by a connection creator.
-     
+
     - parameter connector: The connection creator.
     */
     public func connect<T: protocol<Publisher, Dispatch> where T.Publishing == Publishing>(to connector: T) {
         connect(with: connector.connection(to: self))
     }
-    
+
     /**
      Connects and returns this `StateConnectable` to a connection creator.
-     
+
      - parameter connector: The connection creator.
      - returns: This `StateConnectable` after receiving the connection.
     */
