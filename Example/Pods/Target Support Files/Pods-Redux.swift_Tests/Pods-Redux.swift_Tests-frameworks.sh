b244b0bd0d60bbaa5455bc31270ddf8d7ff560dd
Fellipe Caetano <fellipe.caetano@movile.com>
2016-08-18 13:20:39 -0300

    Removed Quick

--- Example/Pods/Target Support Files/Pods-Redux.swift_Tests/Pods-Redux.swift_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Redux.swift_Tests/Pods-Redux.swift_Tests-frameworks.sh
@@ -86,12 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Redux.swift-RxSwift/Redux.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Redux.swift-RxSwift/Redux.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
 fi
