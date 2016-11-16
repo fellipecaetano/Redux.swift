f3dee7a26038f06073d9d3c4aa6f568c0a3d638b
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-11-16 14:30:25 -0200

    Layout the counter UI

--- Example/Example Application/Source/CounterViewController.swift
+++ Example/Example Application/Source/CounterViewController.swift
@@ -9,7 +9,66 @@ class CounterViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func viewDidLoad() {
-        view.backgroundColor = .white
+    override func loadView() {
+        view = CounterView(frame: UIScreen.main.bounds)
+    }
+}
+
+class CounterView: UIView {
+    let bigDecrementButton = UIButton(type: .system)
+    let smallDecrementButton = UIButton(type: .system)
+    let smallIncrementButton = UIButton(type: .system)
+    let bigIncrementButton = UIButton(type: .system)
+    let label = UILabel()
+
+    var buttons: [UIButton] {
+        return [
+            bigDecrementButton,
+            smallDecrementButton,
+            smallIncrementButton,
+            bigIncrementButton
+        ]
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    private func setup() {
+        backgroundColor = .white
+
+        for button in buttons {
+            button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+            button.setTitle("+1", for: .normal)
+        }
+
+        let buttonsView = UIStackView(arrangedSubviews: buttons)
+        buttonsView.axis = .horizontal
+        buttonsView.alignment = .center
+        buttonsView.spacing = 15
+        buttonsView.distribution = .equalSpacing
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+
+        label.font = UIFont.boldSystemFont(ofSize: 45)
+        label.textAlignment = .center
+        label.text = "0"
+
+        let contentView = UIStackView(arrangedSubviews: [label, buttonsView])
+        contentView.axis = .vertical
+        contentView.alignment = .center
+        contentView.spacing = 100
+        contentView.distribution = .equalSpacing
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(contentView)
+
+        contentView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        contentView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
